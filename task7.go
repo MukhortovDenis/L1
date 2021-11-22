0fff8b2b4b1238f35ed074e25b76073d58525489
Denis Muhortov <muhortovdenis48@gmail.com>
2021-11-22 18:54:54 +0300

    7 задача

---
+++ task7.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var counters = map[int]int{}
+	mu := &sync.Mutex{}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(counters map[int]int, i int, mu *sync.Mutex) {
+			for j := 0; j < 5; j++ {
+				mu.Lock()
+				counters[i+j] = j
+				mu.Unlock()
+			}
+			wg.Done()
+		}(counters, i, mu)
+	}
+	wg.Wait()
+	fmt.Println("Результат", counters)
+}
