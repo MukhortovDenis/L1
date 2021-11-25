9e1a8f7eca5bb07cd445706b43b1600af350e544
Denis Muhortov <muhortovdenis48@gmail.com>
2021-11-25 19:26:13 +0300

    СуперИзи

---
+++ task13.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	list := [10]int{2, 5, 6, 34, 6, 1, 78, 1, 456, 4}
+	fmt.Println("Укажите индекс двух чисел, которых хотите поменять местами")
+	var a, b int
+	fmt.Fscan(os.Stdin, &a, &b)
+	list[a], list[b] = list[b], list[a]
+	fmt.Println(list)
+}
