376a434025808b265ca118e5d35752cb496cc265
Denis Muhortov <muhortovdenis48@gmail.com>
2021-11-26 13:07:36 +0300

    задача16

---
+++ task16.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := []int{23, 52, 154, 1, 65, 76, 89, 34, 22}
+	quicksort(a)
+	fmt.Println(a)
+}
+
+func quicksort(a []int) []int{
+	//Если в слайсе меньше двух элементов, то ничего не делаем
+	//Так же с помощью этой проверки останавливаем рекурсию
+	if len(a) < 2 {
+		return a
+	}
+	var n int
+	l, r := 0, len(a)-1
+	if (l+r)%2 == 0 {
+		n = (l + r) / 2
+	} else {
+		n = (l + r + 1) / 2
+	}
+	a[n], a[r] = a[r], a[n]
+	fmt.Print(a)
+	for i := range a{
+		if a[i] < a[r]{
+			a[l], a[i] = a[i], a[l]
+			l++
+		}
+	}
+	a[l], a[r] = a[r], a[l]
+	fmt.Print(a)
+	//рекурсивно выполняем сортировку для кусков
+	quicksort(a[:l])
+	quicksort(a[l+1:])
+	
+	return a
+}
